35a09dee09b2a9219abe6f9214821cc43b11f812
Jellyfish <anweshkg@gmail.com>
2024-05-20 18:29:32 +0530

    features and getApp

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -4,14 +4,22 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
-  return <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type} >
-    {icon && <Image src={icon} alt={title} width={24} height={24}/>}
-    <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
-  </button>;
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
 };
 
 export default Button;
--- frontend/components/Features.tsx
+++ frontend/components/Features.tsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { FEATURES } from "@/constants";
+import Image from "next/image";
+import React from "react";
 
 const Features = () => {
   return (
-    <div>
-      Features
-    </div>
-  )
-}
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24 mt-[48px]">
+      <div className="max-container padding-container relative w-full flex justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src={"/phone.png"}
+            width={440}
+            height={1000}
+            alt="feature phone"
+            className="feature-phone"
+          />
+        </div>
 
-export default Features
+        <div className="z-20 flex w-full flex-col lg:w-[60%]">
+          <div className="relative">
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+          </div>
+
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+            {FEATURES.map((feature, index) => (
+              <FeatureItem
+                key={feature.title}
+                title={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+type FeatureItemType = {
+  title: string;
+  icon: string;
+  description: string;
+  variant?: string;
+};
+
+const FeatureItem = ({ title, icon, description }: FeatureItemType) => {
+  return (
+    <li className="flex w-full flex-1 flex-col items-start">
+      <div className="rounded-full p-4 lg:p-7 bg-green-50">
+        <Image src={icon} width={28} height={28} alt="feature icon" />
+      </div>
+      <h2 className="bold-20 lg:bold-32 mt-5 capitalize">{title}</h2>
+      <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+        {description}
+      </p>
+      <div className="flex flex-col items-center"></div>
+    </li>
+  );
+};
+
+export default Features;
