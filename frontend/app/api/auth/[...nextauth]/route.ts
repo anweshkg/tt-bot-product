039ea53112d0303ae2d161cdaf4ddaeddc03fb0a
Jellyfish <anweshkg@gmail.com>
2024-06-12 18:02:40 +0530

    minor

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,24 @@
-import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-const GOOGLE_CLIENT_ID = process.env.CLIENT_ID!;
-const GOOGLE_SECRET_ID = process.env.CLIENT_SECRET!;
+// import { NextAuthOptions } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// const GOOGLE_CLIENT_ID = process.env.CLIENT_ID!;
+// const GOOGLE_SECRET_ID = process.env.CLIENT_SECRET!;
 
-const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_SECRET_ID,
-    }),
-  ],
-  callbacks:{
-    async signIn({account, profile}) {
-        if(!profile?.email){
-            throw new Error('No Profile')
-        }
+// const authOptions: NextAuthOptions = {
+//   session: {
+//     strategy: "jwt",
+//   },
+//   providers: [
+//     GoogleProvider({
+//       clientId: GOOGLE_CLIENT_ID,
+//       clientSecret: GOOGLE_SECRET_ID,
+//     }),
+//   ],
+//   callbacks:{
+//     async signIn({account, profile}) {
+//         if(!profile?.email){
+//             throw new Error('No Profile')
+//         }
         
-    }
-  }
-};
+//     }
+//   }
+// };
